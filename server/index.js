8a4fc5e7c1de667997423a8ac91c3b68caa68b96
Yash100177 <yashvirchaudhary100177@gmail.com>
2025-12-19 00:21:41 +0530

    Setup basic Express server and Vite React client

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send("Server is running.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
