f6188eebe0d64f152aaea093ec13e6b56d3e963d
Yash100177 <yashvirchaudhary100177@gmail.com>
2025-12-21 13:43:18 +0530

    setup basic frontend structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h2>Frontend UI starting..</h2>
+      <p>Starting point for the Foodlens</p>
+    </div>
+  );
+};
+
+export default Home;
